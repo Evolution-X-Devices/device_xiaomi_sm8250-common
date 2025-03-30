425abe3f69f8eb97aec572c26f7c4e1c299471e0
TechPanelGM <giomook2003@gmail.com>
2025-03-30 12:12:20 +0000

    sm8250-common: Add target specific kernel configuration

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,8 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_CONFIG := \
     vendor/kona-perf_defconfig \
     vendor/debugfs.config \
-    vendor/xiaomi/sm8250-common.config
+    vendor/xiaomi/sm8250-common.config \
+    vendor/xiaomi/${TARGET_DEVICE}.config
 
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
